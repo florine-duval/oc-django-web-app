0861da0af791115ad763d2606971d232d868709a
florine-duval <florine111@pm.me>
2023-05-27 08:31:40 +0200

    many changes

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,8 +1,46 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Band(models.Model):
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+        ROCK = "RK"
+        JAZZ = 'JZ'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Listing(models.Model):
+
+    class ListingType(models.TextChoices):
+        RECORDS = 'Records'
+        CLOTHING = 'Clothing'
+        POSTERS = 'Posters'
+        MISCELLANEOUS = 'Misc'
+
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(
+        default=1900,
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)]
+    )
+    type = models.fields.CharField(choices=ListingType.choices, max_length=15)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.title}'

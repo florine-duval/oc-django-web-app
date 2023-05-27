d14ae5a4e9fe5fb6ec6365126e39cddd74c6d6b0
florine-duval <florine111@pm.me>
2023-05-27 14:33:00 +0200

    completed app as in the OC course

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,28 +1,126 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from listings.models import Band, Listing
-from listings.forms import ContactUsForm
+from listings.forms import BandForm, ListingForm, ContactUsForm
 
 
 def band_list(request):
     bands = Band.objects.all()
     return render(request, 'listings/band_list.html', {'bands': bands})
 
+
 def band_detail(request, id):
     band = Band.objects.get(id=id)
     return render(request, 'listings/band_detail.html', {'band': band})
 
+
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm()
+    return render(request, 'listings/band_create.html', {'form': form})
+
+
+def band_added(request):
+    return render(request, 'listings/band_added.html')
+
+
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            form.save()
+            return redirect('band-detail', band.id)
+
+    else:
+        form = BandForm(instance=band)
+
+    return render(request, 'listings/band_update.html', {'form': form})
+
+
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+
+    return render(request, 'listings/band_delete.html', {'band': band})
+
+
 def about(request):
     return render(request, 'listings/about.html')
+
 
 def listing_list(request):
     all_listings = Listing.objects.all()
     return render(request, 'listings/listing_list.html', {'listings': all_listings})
 
+
 def listing_detail(request, id):
     listing = Listing.objects.get(id=id)
     return render(request, 'listings/listing_detail.html', {'listing': listing})
 
+
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm()
+    return render(request, 'listings/listing_create.html', {'form': form})
+
+
+def listing_update(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+            return redirect('listing-detail', listing.id)
+
+    else:
+        form = ListingForm(instance=listing)
+
+    return render(request, 'listings/listing_update.html', {'form': form})
+
+
+def listing_delete(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        listing.delete()
+        return redirect('listing-list')
+
+    return render(request, 'listings/listing_delete.html', {'listing': listing})
+
+
 def contact(request):
-    form = ContactUsForm()
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(subject=f'Message from {form.cleaned_data["name"] or "anonymous"} via MerchEx contact form',
+            message=form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['admin@merchex.xyz'])
+            return redirect('email-sent')
+    else:
+        form = ContactUsForm()
     return render(request, 'listings/contact.html', {'form': form})
+
+
+def email_sent(request):
+    return render(request, 'listings/email-sent.html')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -22,8 +22,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bands/', views.band_list, name='band-list'),
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
+    path('bands/add/', views.band_create, name='band-create'),
+    path('bands/added/', views.band_added, name='band-added'),
+    path('bands/<int:id>/update/', views.band_update, name='band-update'),
+    path('bands/<int:id>/delete/', views.band_delete, name='band-delete'),
     path('about-us/', views.about),
     path('listings/', views.listing_list, name='listing-list'),
-    path('listings/<int:id>/', views.listing_detail, name='listing_detail'),
-    path('contact/', views.contact, name='contact')
+    path('listings/<int:id>/', views.listing_detail, name='listing-detail'),
+    path('listings/add/', views.listing_create, name='listing-create'),
+    path('listings/<int:id>/update/', views.listing_update, name='listing-update'),
+    path('listings/<int:id>/delete/', views.listing_delete, name='listing-delete'),
+    path('contact/', views.contact, name='contact'),
+    path('email-sent', views.email_sent, name='email-sent')
 ]
